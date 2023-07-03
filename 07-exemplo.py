db99ba2cf6580a0cfcb8663e5de7dd8949469580
Leo Thomas Monteiro <55034110+anotherleo@users.noreply.github.com>
2023-07-02 23:16:19 -0300

    primeiro contato com a linguagem

---
+++ 07-exemplo.py
@@ -0,0 +1,18 @@
+a = 0
+for i in range(30):
+    if a % 2 == 0:
+        a += 1
+        continue
+    else:
+        if a % 5 == 0:
+            break
+        else:
+            a += 3
+print (a)
+
+# isso é um comentário
+
+"""
+    isso é um comentário 
+    de muitas linhas
+"""
